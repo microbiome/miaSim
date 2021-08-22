27be5aa0d282a587a6eb4c0715fb735413002860
YagmurSimsekk <yagmur.simsek.98@gmail.com>
2021-08-23 02:24:33 +0300

    New function added

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,6 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+library(miaSim)
+x <- miaSim::glv(N = 4, A = powerlawA(n = 4, alpha = 2), tend = 1000)
