cc5e159dc2a07a7a45ec8fddd3d6f992fb8f5af4
YagmurSimsekk <yagmur.simsek.98@gmail.com>
2021-08-24 04:15:17 +0300

    Testing

---
+++ R/function.R
@@ -0,0 +1,37 @@
+setGeneric("glv",signature = c("x"),
+           function(N, A, b = runif(N), x = runif(N), tend = 1000, norm = FALSE)
+             standardGeneric("glv"))
+
+setGeneric("conversionSE",signature = c("x"),
+           function(x, ...)
+             standardGeneric("conversionSE"))
+
+setMethod("glv", signature = c(x="matrix"),
+          function(N, A, b = runif(N), x = runif(N), tend = 1000, norm = FALSE){
+            parameters <- cbind(b, A)
+            times <- seq(0, tend, by = 0.01)
+            out <- ode(
+              y = x,
+              times = times,
+              func = dxdt,
+              parms = parameters
+            )
+            spab <- t(out[,2:ncol(out)])
+            spab <- spab[,round(seq(1, tend*100, length.out = tend))]
+            if(norm){
+              spab <- t(t(spab)/colSums(spab))
+            }
+            return(spab)
+          }
+)
+
+setMethod("conversionSE", signature = c(x="SummarizedExperiment"),
+          function(x){
+            x <- SummarizedExperiment(assays = result)
+          }
+)
+
+result <- glv(N = 4, A = powerlawA(n = 4, alpha = 2), tend = 1000)
+
+final <- conversionSE()
+
