950b71739f2e14fb660558b76a181c3b263c7593
YagmurSimsekk <yagmur.simsek.98@gmail.com>
2021-08-27 04:44:37 +0300

    corrections

--- R/simulateGLV.R
+++ R/simulateGLV.R
@@ -1,55 +1,52 @@
-#' @name simulateGLV
+#' simulateGLV
 #'
-#' @title Simulates time series with the generalized Lotka-Volterra model and forms a
-#'  SummarizedExperiment object.
+#' Simulates time series with the generalized Lotka-Volterra model and forms a
+#' \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
+#' object.
 #'
-#' @description Simulates a community time series using the generalized Lotka-Volterra model,
+#' Simulates a community time series using the generalized Lotka-Volterra model,
 #' defined as dx/dt = x(b+Ax), where x is the vector of species abundances,
 #' A is the interaction matrix and growth_rates the vector of growth rates.
 #'
 #' The abundance matrix that is obtained from the generalized Lotka-Volterra model
-#' is used to construct \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
+#' is used to construct
+#' \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
 #' object
 #'
-#' @param N species number
-#' @param A interaction matrix
-#' @param b growth rates
-#' @param x initial abundances
-#' @param tend timepoints (default: \code{tend = 1000})
-#' @param norm \code{TRUE} or \code{FALSE} : return normalised abundances (proportions in each generation)
-#'  (default: \code{norm = FALSE})
+#' @param N Integer scalar specifying the number of species
+#'
+#' @param A  a interaction matrix
+#'
+#' @param b Numeric scalar indicating growth rates
+#'
+#' @param x Numeric scalar indicating initial abundances
+#'
+#' @param tend Integer scalar indicating timepoints (default: \code{tend = 1000})
+#'
+#' @param norm Logical scalar \code{TRUE} or \code{FALSE}, return normalised abundances
+#'  (proportions in each generation) (default: \code{norm = FALSE})
 #'
 #' @return
-#' \code{simulateGLV} returns a SummarizedExperiment object that has abundance matrix in assay,
-#'   colData that includes sampleID and time, rowData that includes species name, ASV levels,
-#'   and the taxonomic information:
-#' \itemize{
-#'   \item{Kingdom}
-#'   \item{Phylum}
-#'   \item{Class}
+#' \code{simulateGLV} returns a \code{\link{SummarizedExperiment}} object
 #'
 #' @examples
 #' result <- simulateGLV(N = 4, A = powerlawA(n = 4, alpha = 2), tend = 1000)
- NULL
-
-#' @rdname simulateGLV
 #'
 #' @importFrom deSolve ode
 #' @importFrom microsim powerlawA
 #' @importFrom SummarizedExperiment SummarizedExperiment
 #' @export
 
-
 setGeneric("simulateGLV",signature = "N",
            function(N, A, b = runif(N), x = runif(N), tend = 1000, norm = FALSE)
              standardGeneric("simulateGLV"))
 
 .dxdt <- function(t, x, parameters){
-  b <- parameters[,1]
-  A <- parameters[,2:ncol(parameters)]
+    b <- parameters[,1]
+    A <- parameters[,2:ncol(parameters)]
 
-  dx <- x*(b+A %*% x)
-  list(dx)
+    dx <- x*(b+A %*% x)
+    list(dx)
 }
 
 row_data <- data.frame(Kingdom = "A",
@@ -69,28 +66,30 @@ col_data <- data.frame(sampleID = c(1:1000),
                        stringsAsFactors = FALSE)
 
 setMethod("simulateGLV", signature = c(N="numeric"),
-          function(N, A, b = runif(N), x = runif(N), tend = 1000, norm = FALSE){
-            parameters <- cbind(b, A)
-            times <- seq(0, tend, by = 0.01)
+    function(N, A, b = runif(N), x = runif(N), tend = 1000, norm = FALSE){
+          parameters <- cbind(b, A)
+          times <- seq(0, tend, by = 0.01)
 
-            .dxdt(t, x, parameters)
+          .dxdt(t, x, parameters)
 
-            out <- ode(
-              y = x,
-              times = times,
-              func = .dxdt,
-              parms = parameters
-            )
-            spab <- t(out[,2:ncol(out)])
-            spab <- spab[,round(seq(1, tend*100, length.out = tend))]
-            if(norm){
-              spab <- t(t(spab)/colSums(spab))
-              }
-              spab
-              SE <- SummarizedExperiment(assays = spab,
-                                         rowData = row_data,
-                                         colData = col_data)
+          out <- ode(
+          y = x,
+          times = times,
+          func = .dxdt,
+          parms = parameters
+        )
+          spab <- t(out[,2:ncol(out)])
+          spab <- spab[,round(seq(1, tend*100, length.out = tend))]
+          if(norm){
+          spab <- t(t(spab)/colSums(spab))
+        }
+          spab
+          SE <- SummarizedExperiment(assays = spab,
+                                      rowData = row_data,
+                                      colData = col_data)
 
-            return(SE)
+          return(SE)
           }
 )
+
+
